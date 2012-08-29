583788ce813eae0f3912bae3e7981a7a7d5cdbbb
dschrimpf <ds@randi2.de>
2012-08-29 14:59:02 +0200

    changed randomization plugin structure

--- src/main/scala/org/randi3/randomization/BlockRandomizationPlugin.scala
+++ src/main/scala/org/randi3/randomization/BlockRandomizationPlugin.scala
@@ -6,7 +6,8 @@ import org.randi3.randomization.configuration.IntegerConfigurationType
 import org.randi3.randomization.configuration._
 import org.randi3.dao.BlockRandomizationDao
 import org.randi3.model._
-
+import org.randi3.model.criterion.Criterion
+import org.randi3.model.criterion.constraint.Constraint
 import org.scalaquery.ql._
 import org.scalaquery.ql.extended.ExtendedProfile
 import org.scalaquery.session.Database
@@ -18,14 +19,22 @@ import org.randi3.schema.BlockRandomizationSchema._
 
 class BlockRandomizationPlugin(database: Database, driver: ExtendedProfile) extends RandomizationMethodPlugin(database, driver) {
 
-  val name = classOf[BlockRandomization].getName()
+  val name = classOf[BlockRandomization].getName
 
   val i18nName = name
 
-  val BlockRandomizationDao = new BlockRandomizationDao(database, driver)
+  val description = name
 
-  def randomizationConfigurations(): List[ConfigurationType[Any]] = {
-    List(new IntegerConfigurationType(name="blocksize", description="blocksize"))
+  val canBeUsedWithStratification = true
+
+  private val blockRandomizationDao = new BlockRandomizationDao(database, driver)
+
+  def randomizationConfigurationOptions(): (List[ConfigurationType[Any]], List[Criterion[_ <: Any, Constraint[_ <: Any]]]) = {
+    (List(new IntegerConfigurationType(name="blocksize", description="blocksize")), Nil)
+  }
+
+  def getRandomizationConfigurations: List[ConfigurationProperty[Any]] = {
+    Nil
   }
 
   def randomizationMethod(random: RandomGenerator, trial: Trial, configuration: List[ConfigurationProperty[Any]]): Validation[String, RandomizationMethod] = {
@@ -38,23 +47,23 @@ class BlockRandomizationPlugin(database: Database, driver: ExtendedProfile) exte
   }
 
   def create(randomizationMethod: RandomizationMethod, trialId: Int): Validation[String, Int] = {
-    BlockRandomizationDao.create(randomizationMethod.asInstanceOf[BlockRandomization], trialId)
+    blockRandomizationDao.create(randomizationMethod.asInstanceOf[BlockRandomization], trialId)
   }
 
   def get(id: Int): Validation[String, Option[RandomizationMethod]] = {
-    BlockRandomizationDao.get(id)
+    blockRandomizationDao.get(id)
   }
 
   def getFromTrialId(trialId: Int): Validation[String, Option[RandomizationMethod]] = { 
-    BlockRandomizationDao.getFromTrialId(trialId)
+    blockRandomizationDao.getFromTrialId(trialId)
   }
 
   def update(randomizationMethod: RandomizationMethod): Validation[String, RandomizationMethod] = {
-    BlockRandomizationDao.update(randomizationMethod.asInstanceOf[BlockRandomization])
+    blockRandomizationDao.update(randomizationMethod.asInstanceOf[BlockRandomization])
   }
 
   def delete(randomizationMethod: RandomizationMethod) {
-    BlockRandomizationDao.delete(randomizationMethod.asInstanceOf[BlockRandomization])
+    blockRandomizationDao.delete(randomizationMethod.asInstanceOf[BlockRandomization])
   }
 
 }
